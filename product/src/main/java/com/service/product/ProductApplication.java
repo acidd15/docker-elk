1799dc2c81963d01118552a403bc51675c74c30c
IlhoYu <acidrain@wemakeprice.com>
2018-07-24 18:42:17 +0900

    Add services

---
+++ product/src/main/java/com/service/product/ProductApplication.java
@@ -0,0 +1,64 @@
+package com.service.product;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+@Slf4j
+public class ProductApplication {
+	@Data
+	class Product {
+		private String pid;
+		private String name;
+		private String price;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApplication.class, args);
+	}
+
+	@RequestMapping(value = "/products", method = RequestMethod.GET, produces = "application/json")
+	public Object products() {
+
+		log.info("Get products.");
+
+		List<Product> products = new ArrayList<>();
+
+		{
+			Product product = new Product();
+			product.setPid("PID1000");
+			product.setName("Colorful cup");
+			product.setPrice("1000");
+			products.add(product);
+		}
+
+		{
+			Product product = new Product();
+			product.setPid("PID1001");
+			product.setName("Brown Jacket");
+			product.setPrice("1001");
+			products.add(product);
+		}
+
+		{
+			Product product = new Product();
+			product.setPid("PID1002");
+			product.setName("Nice book");
+			product.setPrice("1002");
+			products.add(product);
+		}
+
+		log.info("return products");
+
+		return products;
+	}
+}
---
+++ shop/src/main/java/com/service/shop/ShopApplication.java
@@ -0,0 +1,48 @@
+package com.service.shop;
+
+import brave.Tracer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+@Slf4j
+public class ShopApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ShopApplication.class, args);
+	}
+
+	@Autowired
+	RestTemplate restTemplate;
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@RequestMapping(value = "/products", method = RequestMethod.GET, produces = "application/json")
+	public Object products() {
+		// Don't use as belows. It does not sending the trace-id automatically.
+		// RestTemplate restTemplate = new RestTemplate();
+
+		log.info("Get products from product service.");
+
+		List<Object> products = restTemplate.getForObject("http://localhost:8090/products", List.class);
+
+		log.info("Products received from product service.");
+
+		log.info(products.toString());
+
+		return products;
+
+	}
+}
